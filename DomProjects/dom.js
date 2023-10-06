ad96e37cddf3df5ff9991522c761c8e9fd0fa911
shwetajagtap16 <shwetajagtap5848@gmail.com>
2023-10-06 14:08:03 +0530

    adding dom file

---
+++ DomProjects/dom.js
@@ -0,0 +1,19 @@
+// console.log('hello');
+console.log('hello world');
+// console.log(document.URL);
+console.log(document.body);
+// console.log(document.body);
+// console.log(document.forms);
+// var headerTitle = document.getElementById('header-title');
+// // console.log(headerTitle);
+// headerTitle.style.borderBottom = 'solis 3px #000'
+var addItems= document.getElementsByClassName('list-group-item');
+console.log(addItems);
+// addItems[1].textContent = 'hello'
+// addItems[1].style.fontWeight = 'bold';
+var items = document.getElementsByClassName('title');
+items[0].style.fontWeight='bold';
+addItems[0].style.backgroundColor='yellow';
+items[0].style.color='yellow';
+items[0].style.backgroundColor='grey';
+items[0].style.color='green';
