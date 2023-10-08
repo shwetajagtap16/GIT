e749d79180ed19137655e4ca26eb169528782522
shwetajagtap16 <shwetajagtap5848@gmail.com>
2023-10-08 22:34:59 +0530

    local storage

--- DomProjects/bookingApp.js
+++ DomProjects/bookingApp.js
@@ -1,3 +1,10 @@
 //local storage 
-localStorage.setItem('name','shweta');
-console.log(localStorage.getItem('name'));
+function saveToLocalStorage(event){
+    const name=event.target.name.value;
+    const email= event.target.email.value;
+    localStorage.setItem('name',name);
+    localStorage.setItem('email',email);
+
+
+}
+
