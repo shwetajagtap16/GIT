2207c66786721c5a64d0176b5928d2f887288d9b
shwetajagtap16 <shwetajagtap5848@gmail.com>
2023-10-08 23:26:59 +0530

    storing the user details as an object

--- DomProjects/bookingApp.js
+++ DomProjects/bookingApp.js
@@ -1,10 +1,16 @@
-//local storage 
+// local storage 
 function saveToLocalStorage(event){
     const name=event.target.name.value;
     const email= event.target.email.value;
     localStorage.setItem('name',name);
     localStorage.setItem('email',email);
 
-
+    const user = {
+        name,
+        email,
+      };
+      localStorage.setItem('user', JSON.stringify(user));
 }
+
+
 
