5df4151fd2657410bcec61fa5d7f58f02b902250
shwetajagtap16 <shwetajagtap5848@gmail.com>
2023-10-08 15:25:21 +0530

    added hello to item list

--- DomProjects/dom.js
+++ DomProjects/dom.js
@@ -35,7 +35,81 @@ console.log(secondItemInList);
 secondItemInList.style.backgroundColor='green'*/
 
 
-let odd = document.querySelectorAll('li:nth-child(odd');
+/*let odd = document.querySelectorAll('li:nth-child(odd');
 for(let i=0; i<odd.length; i++){
     odd[i].style.backgroundColor='green';
-}
+}*/
+
+
+//traversing the DOM
+//parentnode
+let itemlist = document.querySelector('#items');
+/*itemlist.parentNode.style.backgroundColor = 'grey';
+console.log(itemlist.parentNode);*/
+
+//parentelement
+/*let itemlist = document.querySelector('#items');
+itemlist.parentElement.parentElement.style.backgroundColor = 'grey';
+console.log(itemlist.parentElement.parentElement.parentElement);*/
+
+//childnode
+//console.log(itemlist.childNodes);
+
+//childrens
+/*console.log(itemlist.children);
+console.log(itemlist.children[1]);
+itemlist.children[1].style.backgroundColor='green';*/
+
+//firstchild
+/*console.log(itemlist.firstChild);*/
+
+//firstelementchild
+/*console.log(itemlist.firstElementChild);
+itemlist.firstElementChild.textContent='Hello 1';*/
+
+//lastchild
+/*console.log(itemlist.lastchild);*/
+
+//lastelementchild
+/*console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.textContent='Hello 4';*/
+
+//nextsibling
+// console.log(itemlist.nextSibling);
+
+//nextelementsibling
+// console.log(itemlist.nextElementSibling);
+
+
+//previousSibling
+// console.log(itemlist.previousSibling);
+
+// previousElementSibling
+// console.log(itemlist.previousElementSibling);
+// itemlist.previousElementSibling.style.backgroundColor = 'green';
+
+
+//createElement
+let newDiv = document.createElement('div');
+console.log(newDiv);
+//add class
+newDiv.className='hello';
+//add id
+newDiv.id ='hello 1';
+//add attributes
+newDiv.setAttribute('title','hello Div')
+//create text node
+let newDivText = document.createTextNode('HEllo ');
+//add text to div
+newDiv.appendChild(newDivText);
+
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+newDiv.style.fontSize='30px';
+
+let firstItem = document.querySelector('#items li');
+let newItem = document.createElement('li');
+newItem.className = 'list-group-item';
+newItem.textContent='HEllo';
+firstItem.parentNode.insertBefore(newItem, firstItem);
