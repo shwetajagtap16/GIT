a0b379f75e9804d4d5f4f3b6043b85f65f8598e1
shwetajagtap16 <shwetajagtap5848@gmail.com>
2023-10-12 13:50:40 +0530

    display user details on screen

--- DomProjects/bookingApp.js
+++ DomProjects/bookingApp.js
@@ -1,16 +1,23 @@
-// local storage 
+//local storage 
 function saveToLocalStorage(event){
+  event.preventDefault();
     const name=event.target.name.value;
     const email= event.target.email.value;
-    localStorage.setItem('name',name);
-    localStorage.setItem('email',email);
+    // localStorage.setItem('name',name);
+    // localStorage.setItem('email',email);
 
     const user = {
         name,
         email,
       };
-      localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem(user.email, JSON.stringify(user));
+      
+      displayUsers(user);
 }
-
-
+function displayUsers(user) {
+  const parentElement = document.getElementById('users');
+  const childElement = document.createElement('li');
+  childElement.textContent= user.name + '-'+ user.email
+  parentElement.appendChild(childElement);
+}
 
