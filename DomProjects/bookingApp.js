7b2d41c1039c358dc14045d36a46c9f978727fff
shwetajagtap16 <shwetajagtap5848@gmail.com>
2023-10-12 14:23:58 +0530

    added edit function to change the details.

--- DomProjects/bookingApp.js
+++ DomProjects/bookingApp.js
@@ -35,7 +35,20 @@ function displayUsers(user) {
     localStorage.removeItem(user.email);
   });
 
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.setAttribute('data-email', user.email);
+  childElement.appendChild(editButton);
+  parentElement.appendChild(childElement);
 
+
+  //event listener for delete button
+  editButton.addEventListener('click',function(){
+    //remove from UI
+    parentElement.removeChild(childElement);
+    //remove form local storage
+    localStorage.removeItem(user.email);
+  });
 }
  
 
